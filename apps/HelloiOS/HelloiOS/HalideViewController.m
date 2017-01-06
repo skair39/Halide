f3de55f852745413a3c6efe6c2952845e7e5c557
Steven Johnson <srj@google.com>
2017-01-05 16:01:58 -0800

    HelloiOS converted to Generator, Metal support added
    (Changes ported from ‘bazel’ branch)

--- apps/HelloiOS/HelloiOS/HalideViewController.m
+++ apps/HelloiOS/HelloiOS/HalideViewController.m
@@ -1,5 +1,6 @@
 //
 //  HalideViewController.m
+//  HelloAppleMetal
 //
 //  Created by Z Stern on 8/10/15.
 //  Copyright (c) 2015 Andrew Adams. All rights reserved.
@@ -12,6 +13,12 @@
 @implementation HalideViewController
 {
     HalideView *_halide_view;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [_halide_view initiateRender];
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
